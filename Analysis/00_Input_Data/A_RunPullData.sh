846f7ba06b8c0b3acef4536e76f00703a3e14464
ssander5 <ss93@iu.edu>
2023-06-13 15:53:22 -0400

    Update A_RunPullData.sh

--- Analysis/00_Input_Data/A_RunPullData.sh
+++ Analysis/00_Input_Data/A_RunPullData.sh
@@ -1,5 +1,6 @@
 #pulling data whole-sale from ../Input_Data.  If you want to import only part of the data, please do that manually.
 
-for f in ../Input_Data/*; do
+for f in ../../Input
+/*; do
    ln -s $f .
 done
