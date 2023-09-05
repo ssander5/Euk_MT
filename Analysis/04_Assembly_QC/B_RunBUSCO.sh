d581d6349089ba412c3f4d9896656953ac437d70
ssander5 <ss93@iu.edu>
2023-09-05 18:04:09 -0400

    Update B_RunBUSCO.sh

--- Analysis/04_Assembly_QC/B_RunBUSCO.sh
+++ Analysis/04_Assembly_QC/B_RunBUSCO.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+#SBATCH -J RunAsmblyQC_B
+#SBATCH -p RM-shared
+#SBATCH -o %j.txt
+#SBATCH -e %j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=2
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=05:00:00
 
 busco \
    -m transcriptome \
