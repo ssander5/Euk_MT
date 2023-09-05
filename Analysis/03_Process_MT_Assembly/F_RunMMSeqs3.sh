5576b5a86ef588420a43517a6ce58a9f2f2a87ce
ssander5 <ss93@iu.edu>
2023-09-05 18:04:48 -0400

    Update F_RunMMSeqs3.sh

--- Analysis/03_Process_MT_Assembly/F_RunMMSeqs3.sh
+++ Analysis/03_Process_MT_Assembly/F_RunMMSeqs3.sh
@@ -17,8 +17,8 @@ in=
 mmseqs createdb $in $in.db
 mmseqs linclust $in.db $in.tmp tmp --min-seq-id 98 --cov-mode 1 -c 1000 2>1& > mmseqs.log
 mmseqs createsubdb $in.tmp $in.db $in.2.db
-mmseqs convert2fasta $in.2.db 98cluster.fasta
-mmseqs createsv $in.db $in.db $in.tmp 98cluster.tsv
+mmseqs convert2fasta $in.2.db Assembly.clean.fasta
+mmseqs createsv $in.db $in.db $in.tmp Assembly.clean.tsv
 
 rm -rf $in.db*
 rm -rf $in.tmp*
