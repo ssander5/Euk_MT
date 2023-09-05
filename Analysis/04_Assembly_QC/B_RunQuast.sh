08d17f35b59de15f274fe62da40f4177cb257887
ssander5 <ss93@iu.edu>
2023-09-05 18:05:13 -0400

    Update B_RunQuast.sh

--- Analysis/04_Assembly_QC/B_RunQuast.sh
+++ Analysis/04_Assembly_QC/B_RunQuast.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+#SBATCH -J RunAsmblQC_B
+#SBATCH -p RM-shared
+#SBATCH -o %j.txt
+#SBATCH -e %j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=2
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=05:00:00
+
 quast.py ../03_Process_MT_Assembly/Assembly.clean.fa
